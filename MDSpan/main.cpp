533dc763c85789852eb94be196017e76299d912f
KovarJoel <joel.m.kovar@gmail.com>
2024-02-02 15:57:00 +0100

    Moved comments for review to main.

--- MDSpan/main.cpp
+++ MDSpan/main.cpp
@@ -20,3 +20,14 @@ int main() {
 
  	return 0;
 }
+
+/*
+* Should `MDSpan::operator[]` be const? It doesn't modify `*this`, however you still get access to the "pointees".
+* Same for `begin()` and `end()`.
+* Easier way for `DropFirstPackValue`, is there something similar in the standard?
+* Is the `const_cast` in `at()` well defined, is there a cleaner way?
+* Is there an alternative to marking all methods as `constexpr`?
+* Is it ok that `Iterator::operator*` and `MDSpan::operator[]` return new `MDSpan`s and not refernences (its a view anyways -> lightweight)?
+* Should there be bounds checking (needs an `end` pointer)? If so, rather use `assert()` or exceptions (debug or always)?
+* Should the bounds checking only perform "start <= iterator < end", or check for the strides as well?
+*/
