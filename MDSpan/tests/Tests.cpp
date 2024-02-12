fd042548bcd3407e485f955dd2dd10f0374196ec
KovarJoel <joel.m.kovar@gmail.com>
2024-02-12 18:41:21 +0100

    added a test for access

--- MDSpan/tests/Tests.cpp
+++ MDSpan/tests/Tests.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <string>
 #include <array>
+#include <numeric>
+#include <functional>
 
 #include "../src/MDSpan.h"
 
@@ -119,6 +121,74 @@ static bool testDimensionsAndStrides() {
 
 	return true;
 }
+static bool testAccess() {
+	std::array<int, 9> arr{ 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+	MDSpan<int, 3, 3> span{ arr.data() };
+
+	for (std::size_t i{}; i < arr.size(); ++i) {
+		if (arr[i] != span[0][i]
+			|| arr[i] != span.at(0ull, i)
+			|| arr[i] != span[0].at(i)) {
+			return false;
+		}
+	}
+
+	for (std::size_t i{}; i < 3; ++i) {
+		for (std::size_t j{}; j < 3; ++j) {
+			if (arr[i * 3 + j] != span[i][j]
+				|| arr[i * 3 + j] != span.at(i, j)
+				|| arr[i * 3 + j] != span[i].at(j)) {
+				return false;
+			}
+		}
+	}
+
+#define INDICES_DATA 100, 8, 5, 25, 2
+	constexpr std::size_t vecSize{ 200'000 };
+	std::vector<int> vec{};
+	vec.resize(vecSize);
+	for (std::size_t i{}; i < vecSize; ++i) {
+		vec[i] = static_cast<int>(i);
+	}
+
+	constexpr int indices[] = { INDICES_DATA };
+	static_assert(std::accumulate(std::begin(indices), std::end(indices), 1, std::multiplies{}) == vecSize);
+	MDSpan<int, INDICES_DATA> hugeSpan{ vec.data() };
+	for (int a = 0; a < indices[0]; ++a) {
+		for (int b = 0; b < indices[1]; ++b) {
+			for (int c = 0; c < indices[2]; ++c) {
+				for (int d = 0; d < indices[3]; ++d) {
+					for (int e = 0; e < indices[4]; ++e) {
+						int offset = 0;
+						offset += e * std::accumulate(std::rbegin(indices), std::rbegin(indices) + 0, 1, std::multiplies{});
+						offset += d * std::accumulate(std::rbegin(indices), std::rbegin(indices) + 1, 1, std::multiplies{});
+						offset += c * std::accumulate(std::rbegin(indices), std::rbegin(indices) + 2, 1, std::multiplies{});
+						offset += b * std::accumulate(std::rbegin(indices), std::rbegin(indices) + 3, 1, std::multiplies{});
+						offset += a * std::accumulate(std::rbegin(indices), std::rbegin(indices) + 4, 1, std::multiplies{});
+
+						int subscript = hugeSpan[a][b][c][d][e];
+						int at = hugeSpan.at(a, b, c, d, e);
+						int both = hugeSpan[a][b][c].at(d, e);
+
+						const auto& cref = hugeSpan;
+						int constAt = cref.at(a, b, c, d, e);
+						// operator[] is not const for multidimensionals!
+
+						if (vec[offset] != subscript
+							|| vec[offset] != at
+							|| vec[offset] != both
+							|| vec[offset] != constAt) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+	}
+#undef INDICES_DATA
+
+	return true;
+}
 
 bool passedAllTests() {
 	std::size_t index{};
@@ -128,6 +198,7 @@ bool passedAllTests() {
 	TEST(testDataAndReset);
 	TEST(testEquality);
 	TEST(testDimensionsAndStrides);
+	TEST(testAccess);
 
 	return !failed;
 }
